9ed187c8e0dc62db20f18fff86f922cedeb80b3d
nikunj2706 <nikunjsth5@gmail.com>
2023-01-14 13:31:42 +0000

    basic UI functionalities

--- Assets/Scripts/PlayerDamage.cs
+++ Assets/Scripts/PlayerDamage.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerDamage : MonoBehaviour
 {
+    public Image healthHurt;
+    public Image healthBar;
+    public AudioSource _playerInjuredAudio;
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("PlayerBody"))
         {
-            Debug.Log("HittingPlayer");
+            StartCoroutine(PlayerHurt());
         }
+    }
+
+    IEnumerator PlayerHurt()
+    {
+        healthHurt.enabled = true;
+        _playerInjuredAudio.Play();
+        healthBar.fillAmount = healthBar.fillAmount - 0.1f;
+        yield return new WaitForSeconds(3f);
+        healthHurt.enabled = false;
     }
 }
